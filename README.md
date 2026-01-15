51e12bed583b623f6620b63fdcf64350bbddc9a6
chaitu752005 <shimpichaitanya22@gmail.com>
2026-01-15 21:04:39 +0530

    Added Task 1 files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# python-task-1
-Python Internship Task 1
+# Python Internship Task 1
+
+This task demonstrates:
+- Python environment setup
+- Basic input and output
+- Variables usage
+- Displaying today's date
+
+Internship Role:
+Python Developer Intern
+
+Tools Used:
+- Python
+- VS Code
+
+Output screenshot is included.
---
+++ hello_world.py
@@ -0,0 +1,15 @@
+
+# taking input from user
+name = input("Enter your name: ")
+role = input("Enter your internship role: ")
+
+# importing datetime module
+from datetime import date
+
+# getting today's date
+today = date.today()
+
+# printing output
+print("Name:", name)
+print("Internship Role:", role)
+print("Today's Date:", today)
